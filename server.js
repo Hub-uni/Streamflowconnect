1e93fdcc69fc553f86ecb716a56fdf91c1a6fc7d
Hub-uni <noreply@getunihub.com>
2026-08-27 07:38:48 -0700

    Fix static file path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/api/states", async (req, res) => {
     try {
